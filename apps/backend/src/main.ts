0bdb8856e0f9f095264a717473838b42de0ce36b
jmasciar82 <juanpsnusa@gmail.com>
2026-05-25 20:01:25 -0300

    cambios en el so 2

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3001);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3002',
+    ],
+    credentials: true,
+  });
+
+  await app.listen(3001);
 }
-void bootstrap();
+
+bootstrap();
